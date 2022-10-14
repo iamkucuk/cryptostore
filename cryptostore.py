060c6152738223f0756560ef69c632d97d228735
Furkan Kucuk <furkankucuk@outlook.com>
2022-10-15 01:45:16 +0300

    updoot

--- cryptostore.py
+++ cryptostore.py
@@ -133,7 +133,7 @@ def load_config() -> Feed:
     elif backend == 'KAFKA':
         # args = {'bootstrap': host, 'port': port if port else 29092}
         # print(f'{host}:{port}')
-        args = (['redpanda:29092', ])
+        args = ('redpanda', '29092')
         cbs = {
             L2_BOOK: BookKafka(*args, snapshot_interval=snap_interval, snapshots_only=snap_only),
             TRADES: TradeKafka(*args),
